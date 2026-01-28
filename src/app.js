d1c24b59eda7ab27799791641f6dbcdba198c34b
Manish Neupane <2naligaming2002@gmail.com>
2026-01-27 21:57:27 -0500

    Break formaat to test CI

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ app.get("/", (req, res) => {
   res.json({ status: "ok", message: "Hello from CSP451" });
 });
 app.get('/health', (req, res) => {
- res.stat .json({status:'healthy',uptime:process.uptime});
+res.status(200).json({ status: "healthy", uptime: process.uptime()           });
+
  
 });
 
