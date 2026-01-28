8cb4ab6856243ca632b3980399f6cbeb26617f32
Manish Neupane <2naligaming2002@gmail.com>
2026-01-27 22:32:57 -0500

    Break formating to test CI

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.json({ status: "ok", message: "Hello from CSP451" });
 });
 
-module.exp orts = app;
+module.exports = app;
 
 // Allow running locally: `npm start`
 if (require.main === module) {
